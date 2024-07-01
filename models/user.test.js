594080b3bedbc0e9418cd22a602dfa583a25d320
Michael Lichtenberger <mikelichtenberger81@gmail.com>
2024-06-30 20:49:10 -0400

    models

--- models/user.test.js
+++ models/user.test.js
@@ -12,7 +12,10 @@ const {
   commonBeforeEach,
   commonAfterEach,
   commonAfterAll,
+  testJobIds,
 } = require("./_testCommon");
+const { describe, default: test } = require("node:test");
+const { fail } = require("assert");
 
 beforeAll(commonBeforeAll);
 beforeEach(commonBeforeEach);
@@ -140,6 +143,7 @@ describe("get", function () {
       lastName: "U1L",
       email: "u1@email.com",
       isAdmin: false,
+      applications: [testJobIds[0]],
     });
   });
 
@@ -228,3 +232,34 @@ describe("remove", function () {
     }
   });
 });
+
+describe('applyToJob', function () {
+  test('works', async function () {
+    await User.applyToJob('u1', testJobIds[1])
+
+    const res = await db.query(
+      `SELECT * FROM applications WHERE job_id=$1`, [testJobIds[1]])
+    expect(res.rows).toEqual([{
+      job_id: testJobIds[1],
+      username: 'u1',
+    }])
+  })
+
+  test('not found if no such job', async function () {
+    try {
+      await User.applyToJob('u1', 0, 'applies')
+      fail()
+    } catch (e) {
+      expect(e instanceof NotFoundError).toBeTruthy()
+    }
+  })
+
+  test('not found in no such user', async function() {
+    try {
+      await User.applyToJob('nope', testJobIds[0], 'applied')
+      fail()
+    } catch (e) {
+      expect(e instanceof NotFoundError).toBeTruthy()
+    }
+  })
+})
